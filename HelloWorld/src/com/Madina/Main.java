78baf64e7ae3a34de8018a2f1be28df36ddfc41f
Madina <madina2200@gmail.com>
2020-08-31 04:46:29 +0600

    HW1

--- HelloWorld/src/com/Madina/Main.java
+++ HelloWorld/src/com/Madina/Main.java
@@ -3,7 +3,8 @@ package com.Madina;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("                __" + "\t\t-------------------\n" +
+        System.out.println("" +
+                "                __" + "\t\t-------------------\n" +
                 "               /\\/'-," + "   ||Welcome to SJSU||\n" +
                 "       ,--'''''   /\"\t-------------------\n" +
                 " ____,'.  )       \\___\n" +
